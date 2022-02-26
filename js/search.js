6c29550b95559d3e604e00e67ccc12079ab0d824
Skmm6 <egor.qaq@gmail.com>
2022-02-26 17:32:17 +0300

    first commit

---
+++ js/search.js
@@ -0,0 +1,11 @@
+const search = function () {
+    const input = document.querySelector('.search-block > input')
+    const searchBtn = document.querySelector('.search-block > button')
+
+    searchBtn.addEventListener('click', () => {
+        console.log(input.value);
+    })
+    console.log(searchBtn);
+}
+
+search()
