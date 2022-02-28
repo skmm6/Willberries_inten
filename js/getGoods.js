e461d953788f59ca2eb753f80ed843a791241f6a
Skmm6 <egor.qaq@gmail.com>
2022-02-28 19:09:59 +0300

    day3

--- js/getGoods.js
+++ js/getGoods.js
@@ -1,20 +1,75 @@
 const getGoods = () => {
     const links = document.querySelectorAll('.navigation-link');
+    const viewAll = document.querySelector('.more')
 
-    const getData = () => {
-        fetch('https://willberris-17c9c-default-rtdb.europe-west1.firebasedatabase.app/db.json')
-        .then((response) => response.json())
-        .then((data) => {
-            localStorage.setItem('goods', JSON.stringify(data));
+    const renderGoods = (goods) => {
+        const goodsContainer  = document.querySelector('.long-goods-list')
+
+        goodsContainer.innerHTML = " "
+
+        goods.forEach(good => {
+             const goodBlock = document.createElement('div')
+
+             goodBlock.classList.add('col-lg-3')
+             goodBlock.classList.add('col-sm-6')
+
+             goodBlock.innerHTML = `
+                <div class="goods-card">
+                    <span class="label ${good.label ? null : 'd-none'}">${good.label}</span>
+                    <img src="db/${good.img}" alt="${good.name}" class="goods-image">
+                    <h3 class="goods-title">${good.name}</h3>
+                    <p class="goods-description">${good.description}</p>
+                    <button class="button goods-card-btn add-to-cart" data-id="${good.id}">
+                        <span class="button-price">$${good.price}</span>
+                    </button>
+                </div>
+             `
+             goodsContainer.append(goodBlock)
         })
     }
 
-    links.forEach((item) => {
-        item.addEventListener('click', (e) => {
-            e.preventDefault();
-            getData();
+    const getData = (value, category) => {
+        fetch('https://willberris-17c9c-default-rtdb.europe-west1.firebasedatabase.app/db.json')
+        .then((response) => response.json())
+        .then((data) => {
+            const array = category ? data.filter((item) => item[category] === value) : data
+
+            localStorage.setItem('goods', JSON.stringify(array));
+
+            if (window.location.pathname !== "/goods.html"){
+                window.location.href = '/goods.html'
+            } else {
+                renderGoods(array)
+            }
+
+            
         })
-    })
+    }
+
+    links.forEach((link) => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            const linkValue = link.textContent
+            const category = link.dataset.field
+           
+
+            getData(linkValue, category);
+        })
+    }) 
+    if (localStorage.getItem('goods') && window.location.pathname === "/goods.html") {
+        renderGoods(JSON.parce(localStorage.getItem('goods')))
+    }
+    
+    try {
+        viewAll.addEventListener('click', (event) => {
+            event.preventDefault()
+            const linkValue = viewAll.textContent
+            const category = viewAll.dataset.field
+            getData(linkValue, category)
+        })
+    } catch (e) {
+        console.dir(e)
+    }
 }
 
 getGoods();
